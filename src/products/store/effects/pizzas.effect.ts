1efbaba68f940197fef6f7b3857b264ee1ce94fa
Brian Diesel <brian@designcondition.com>
2020-04-24 16:30:30 -0400

    Routing via dispatch complete

--- src/products/store/effects/pizzas.effect.ts
+++ src/products/store/effects/pizzas.effect.ts
@@ -4,6 +4,7 @@ import { Effect, Actions } from '@ngrx/effects';
 import { of } from 'rxjs/observable/of';
 import { map, switchMap, catchError } from 'rxjs/operators'; 
 
+import * as fromRoot from '../../../app/store';
 import * as pizzaActions from '../actions/pizzas.actions';
 import * as fromServices from '../../services';
 
@@ -38,6 +39,18 @@ export class PizzasEffects {
     );
 
     @Effect()
+    createPizzaSuccess$ = this.actions$
+        .ofType(pizzaActions.CREATE_PIZZA_SUCCESS)
+        .pipe(
+            map((action: pizzaActions.CreatePizzaSuccess) => action.payload),
+            map(pizza => {
+                return new fromRoot.Go({
+                    path: ['/products', pizza.id]
+                });
+            })
+        );
+
+    @Effect()
     updatePizza$ = this.actions$.ofType(pizzaActions.UPDATE_PIZZA).pipe(
         map((action: pizzaActions.UpdatePizza) => action.payload),
         switchMap(pizza => {
@@ -62,4 +75,19 @@ export class PizzasEffects {
             )
         })
     )
+
+
+    @Effect()
+    handlePizzaSuccess$ = this.actions$
+        .ofType(
+            pizzaActions.UPDATE_PIZZA_SUCCESS,
+            pizzaActions.REMOVE_PIZZA_SUCCESS,
+        )
+        .pipe(
+            map(pizza => {
+                return new fromRoot.Go({
+                    path: ['/products']
+                })
+            })
+        )
 } 
